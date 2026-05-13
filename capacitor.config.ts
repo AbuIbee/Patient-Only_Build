760887c32e7b23f17b12b3270bd971112c7e3a5f
AbuIbee <solodeen@hotmail.com>
2026-05-13 16:50:15 -0400

    My latest updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mymemoriaally.app',
-  appName: 'MyMemoriaAlly',
+  appId: 'com.memoriahelps.ap',
+  appName: 'MemoriaHelps',
   webDir: 'dist'
 };
 
