8c591fe9abe9d9375f26f8ff6f57b4944d4da46f
AbuIbee <solodeen@hotmail.com>
2026-06-17 09:05:40 -0400

    Latest updates and gameing features

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.memoriahelps.ap',
-  appName: 'MemoriaHelps',
-  webDir: 'dist'
+  appName: 'MyMemoriaAlly',
+  webDir: 'dist',
+  server: {
+    url: 'https://www.mymemoriaally.com',
+    androidScheme: 'https',
+    allowNavigation: ['www.mymemoriaally.com']
+  },
+  // ... any other existing configuration
 };
 
 export default config;
